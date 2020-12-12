7be109bb8f82d4e59ec9fcafeb89a1df5f59715c
Eternal134 <18756762798@163.com>
2020-12-13 00:13:21 +0800

    python课程完成！

--- python_course_design/AnalysePd.py
+++ python_course_design/AnalysePd.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from pandas.plotting import table
 from IPython.display import display
 from matplotlib import pyplot as plt
 from collections import Counter
+
 ''' 设置pandas的输出样式 '''
 pd.set_option('display.unicode.ambiguous_as_wide', True)
 pd.set_option('display.unicode.east_asian_width', True)
@@ -17,19 +17,21 @@ unit_price_pd = pd.Series(all_items_pd['价格'] / all_items_pd['页数'], name=
 # 连接到总数据中
 all_items_pd = pd.concat([all_items_pd, unit_price_pd], axis = 1)
 
+print(all_items_pd.loc[all_items_pd['页均价'] >= 2]['名称'])
 
 def getYear(x):
+    ''' 从数据中的出版日期项中获取年份，如果出版日期的格式不是"年-月-日"或者"年年月月日日"形式，直接舍弃该样本 '''
     if '-' in x:
         return x.split('-')[0]
     elif '年' in x:
         return x.split('年')[0]
     return ""
+# 将所有的出版日期修改成年份
 all_items_pd['出版日期'] = all_items_pd['出版日期'].apply(getYear)
 
-xlsl = pd.ExcelWriter('统计数据.xls')
-
-def getStat(direction):
-    """ 根据direction获取想要的统计数据，比如direction是图书类型，则此函数可以统计与图书类型有关的统计数据 """
+def getStat(direction, index, length):
+    """ 根据direction获取想要的统计数据，比如direction是图书类型，则此函数可以统计与图书类型有关的统计数据
+    index是direction在所有要统计类型中的下标，length是所有要统计方向的个数，这两个参数主要用于绘制子图 """
     # 设置感兴趣的统计类型
     stat_types = ['页均价', '中位数', '标准差', '样本数']
     """ 先获取统计方向direction上的出现频率最高的10个分量，如果direction是出版日期，则统计前20个分量 """
@@ -39,17 +41,18 @@ def getStat(direction):
         top_direction = [i[0] for i in Counter(all_items_pd[direction]).most_common(20) if i[0]]
         # 按年份递增排序
         top_direction.sort(key = lambda x: int(x))
+    # print(top_direction)
     # 表示统计数据的DataFrame对象，每行是一个direction的一个分量，各列的含义见stat_types
     stat_pd = pd.DataFrame(np.zeros((len(top_direction), len(stat_types))), index = top_direction, columns = stat_types)
     
-    for index in stat_pd.index:
+    for _index in stat_pd.index:
         ''' 遍历stat_pd的每个行索引，对每个index: 
         all_items_pd.loc[all_items_pd[direction] == index]可以索引到all_items_pd中所有direction等于index的行，
         再索引'页均价'列，最后对索引到的结果调用求均值和求标准差方法 '''
-        stat_pd.loc[index][stat_types[0]] = all_items_pd.loc[all_items_pd[direction] == index]['页均价'].mean()
-        stat_pd.loc[index][stat_types[1]] = all_items_pd.loc[all_items_pd[direction] == index]['页均价'].median()
-        stat_pd.loc[index][stat_types[2]] = all_items_pd.loc[all_items_pd[direction] == index]['页均价'].std()
-        stat_pd.loc[index][stat_types[3]] = all_items_pd.loc[all_items_pd[direction] == index]['页均价'].size
+        stat_pd.loc[_index][stat_types[0]] = all_items_pd.loc[all_items_pd[direction] == _index]['页均价'].mean()
+        stat_pd.loc[_index][stat_types[1]] = all_items_pd.loc[all_items_pd[direction] == _index]['页均价'].median()
+        stat_pd.loc[_index][stat_types[2]] = all_items_pd.loc[all_items_pd[direction] == _index]['页均价'].std()
+        stat_pd.loc[_index][stat_types[3]] = all_items_pd.loc[all_items_pd[direction] == _index]['页均价'].size
     print(stat_pd)
     """ 绘图 """
     # 设置正常显示中文
@@ -59,16 +62,36 @@ def getStat(direction):
     dpi = 400
 
     title = direction + "与页均价统计关系"
+    # 长度超过10个字符的标签，只取前10个字符，后面的用省略号代替，使绘图结果更加美观
+    top_direction = [x[:10] + '...' if len(x) > 10 else x for x in top_direction]
     # 绘制前3列，最后一列的样本数不绘制图形
     stat_pd.iloc[:, :3].plot()
+    # 设置标题
     plt.title(title)
-    plt.xticks(range(len(top_direction)), top_direction, rotation = -60)
+    # 设置横坐标标签的样式
+    plt.xticks(range(len(top_direction)), top_direction, rotation = -90)
+    # 设置网格的样式
     plt.grid(ls = '-.')
+    # 使用此方法可以完整的显示图像
     plt.tight_layout()
+    # 保存
     plt.savefig(title + '.png', dpi = dpi)
-    # 保存excel文件
-    stat_pd.to_excel(xlsl)
+    # 关闭此图，方便后面绘图
+    plt.close()
+    ''' 绘制样本个数的饼图，所有统计方向上的饼图以子图的形式绘制在同一个figure上 '''
+    plt.figure(2)
+    # 绘制子图的位置
+    s = plt.subplot(int(length ** 0.5), int(length ** 0.5), index + 1)
+    ''' explode是在饼图中想要突出显示的某块，在每个图中都突出显示占比最高的那个 '''
+    if direction != '出版日期':
+        explode = [0 if x != 0 else 0.1 for x in range(len(top_direction))]
+    else:
+        explode = [0 if x != len(top_direction) - 1 else 0.1 for x in range(len(top_direction))]
+    # 绘制饼图
+    stat_pd.iloc[:, -1].plot.pie(autopct='%.2f%%', fontsize = 5, explode = explode)
 
 directions = ['图书类型', '作者', '出版公司', '出版日期']
 for d in directions:
-    getStat(d)
+    getStat(d, directions.index(d), len(directions))
+# 保存饼图，此时已是绘制完所有子图的完整图形
+plt.savefig("样本个数饼图.png", dpi = 400)
--- python_course_design/DoubanSpider.py
+++ python_course_design/DoubanSpider.py
@@ -9,13 +9,11 @@ class DoubanSpider():
     
     def __init__(self):
         
-        self.s = requests.session()
-        self.url = "https://book.douban.com/"
         self.basic_tag_url = "https://book.douban.com/tag/"
     
     def getTagBooks(self, tag, num):
         """根据书籍标签tag抓取num本书"""
-        index = 500
+        index = 600
         i = 0
         for page_start in range(index, num + index, 20):
             # 要爬取的网址实例：https://book.douban.com/tag/文学?start=page_start/
@@ -25,32 +23,65 @@ class DoubanSpider():
             headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
                 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.120 Safari/537.36',
                 }
-            response = requests.get(url, headers = headers)
+            # 提交网络请求
+            response = requests.get(url, headers = headers, timeout = 2)
+            # 生成etree对象
             r_html = etree.HTML(response.content)
+            # 获取页面上所有的li标签
             item_list = r_html.xpath("//li[@class='subject-item']")
             for item in item_list:
                 book = [tag]
                 info = item.xpath("./div[2]")[0]
                 try:
+                    ''' 有些书籍的页面很不规则，如果在提取信息的时候发生错误，选择直接舍弃掉该本书 '''
+                    # 书名
                     book.append(info.xpath("./h2/a/text()")[0].strip())
+                    # 评分
                     book.append(info.xpath("./div/span[@class='rating_nums']/text()")[0])
+                    # 作者
                     book.append(info.xpath("./div[@class='pub']/text()")[0].split('/')[0].replace('\n', '').strip())
-                # 进入详情页获取更多信息
+                    # 详情页网址
                     content_url = item.xpath("./div[@class='info']/h2/a/@href")[0]
                     content = etree.HTML(requests.get(content_url, headers = headers).content)
                     info = content.xpath("//div[@id='info']")[0]
-                    # book['writer'] = info.xpath("./a[@href]/text()")[0]
+                    ''' 提取感兴趣的信息 '''
                     info_str = etree.tostring(info, encoding = 'utf-8').decode()
+                    # 出版社
                     book.append(re.findall(r'<span class="pl">出版社:</span> (.*)<br/>', info_str)[0])
+                    # 出版日期
                     book.append(re.findall(r'<span class="pl">出版年:</span> (.*)<br/>', info_str)[0])
-                    book.append(re.findall(r'<span class="pl">定价:</span>\D+(\d+\.?\d+).*<br/>', info_str)[0])
+                    # 价格
+                    price = re.findall(r'<span class="pl">定价:</span> (.*)<br/>', info_str)[0]
+                    try:
+                        # 如果price不是纯数字，此处会报错，如果是纯数字，则保留这个值
+                        float(price)
+                        book.append(price)
+                    except:
+                        # 如果不是纯数字而且price里没有'元'，大概率不是人民币价格，舍弃掉此样本
+                        if '元' in price:
+                            book.append(re.findall(r'(\d+.?\d+)', price)[0])
+                        else:
+                            continue
+                    # 页数
                     book.append(re.findall(r'<span class="pl">页数:</span> (\d+).*<br/>', info_str)[0])
-                    
+                    ''' 写入文件 '''
                     file = open('book_item.csv', 'a+', newline = '', encoding='utf-8')
                     writer = csv.writer(file)
                     writer.writerow(book)
                     file.close()
+                    # 打印本次爬取了多少本书
                     print(i)
                     i += 1
                 except:
                     continue
+                
+if __name__ == "__main__":
+
+    # 要爬取图书的总量
+    books_sum = 4000
+    spider = DoubanSpider()
+    # book_types = ['推理', '文学', '诗歌', '计算机', '科普', '爱情', '心理', '经济学', '历史', '漫画']
+    book_types = ['漫画']
+    books = []
+    for tag in book_types:
+        books = spider.getTagBooks(tag, int(books_sum / len(book_types)))
